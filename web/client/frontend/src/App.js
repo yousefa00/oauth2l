02cf8baa64f59f6b42504636cadb2c4b72f750c5
LeoRuiz01 <lruiz0333@gmail.com>
2020-06-17 10:53:19 -0500

    add new icon to radio buttons and spacing between rows

--- web/client/frontend/src/App.js
+++ web/client/frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
       <MDBNavbar color="blue">
         <MDBNavbarBrand>
-        <img src={process.env.PUBLIC_URL + "/clogo.png"} width = "250" />
+        <img src={process.env.PUBLIC_URL + "/clogo.png"} width = "250" alt="This is a logo for Google Cloud" />
     
         </MDBNavbarBrand>
       </MDBNavbar>
--- web/client/frontend/src/components/Credentials.js
+++ web/client/frontend/src/components/Credentials.js
@@ -1,19 +1,25 @@
 import React,{useState} from 'react';
 import Button from './Button';
-import { MDBContainer, MDBRow,MDBCol,MDBBtn,MDBNav } from 'mdbreact';
+import { MDBContainer, MDBRow,MDBCol,MDBBtn,MDBIcon} from 'mdbreact';
 
 
 function Credentials(){
-   const [type,setType] = useState("OAuth");
-   const [format, setFormat] = useState("Bare");
+   const [type,setType] = useState("");
+   const [format, setFormat] = useState("");
+   const formatChange = (e) => setFormat(e.target.value); //refactoring code 
+   const typeChange = (e) => setType(e.target.value); //refactoring code 
+   
+
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      if (e.target.format.value === "Header" && e.target.type.value === "JWT"){
-         alert('CANNOT DO THIS');//reset format and header if so
-      }
+      if (e.target.format.value === "Header" && e.target.type.value === "JWT"){ 
+         alert('These two values cannot be paired together, please change them');//don't send the values to the next page and ask the user to change them
+
+      }else{
       console.log(e.target.type.value);
       console.log(e.target.format.value);
+      }
    }
 
    return (
@@ -21,28 +27,43 @@ function Credentials(){
     <div className= "shadow-box-example z-depth-2">
     <MDBContainer>
     <form onSubmit={handleSubmit}> 
-    <MDBRow> 
-    <MDBCol size="9"> <p className= "h5 text-center mb-4">Choose token type </p>
-    </MDBCol></MDBRow> 
-    <MDBRow><fieldset id='type'>
-    <MDBCol><input type = "radio" name = "type" value = "OAuth"onChange={e => setType(e.target.value)}/><label>OAuth</label> </MDBCol>	
-    <MDBCol><input type = "radio" name = "type" value = "JWT" onChange={e => setType(e.target.value)}/><label>JWT</label></MDBCol></fieldset></MDBRow>
+    
+    <MDBCol> <p className= "h5 text-center mb-4">Choose token type </p>
+    </MDBCol>
+    <fieldset id='type'>
+    <MDBRow>
+    <MDBCol><div className="custom-control custom-radio"><input type = "radio" name = "type" value = "OAuth" className= "custom-control-input" id="defaultGroupExample1" onChange={typeChange}/><label className="custom-control-label" for="defaultGroupExample1">OAuth</label></div></MDBCol>	
+    <MDBCol><div className="custom-control custom-radio"><input type = "radio" name = "type" value = "JWT"  className= "custom-control-input" id="defaultGroupExample2"onChange={typeChange}/><label className="custom-control-label" for="defaultGroupExample2">JWT</label></div> </MDBCol>
+    </MDBRow>
+    </fieldset>
+    
     
    
-    <MDBCol size="9"> <p className= "h5 text-center mb-4">Choose token format </p></MDBCol> 
+    <MDBCol> <p className= "h5 text-center mb-4">Choose token format </p></MDBCol> 
     <fieldset id= "format">
-    <MDBCol><input type = "radio" name = "format" value = "Bare" onChange={e => setFormat(e.target.value)}/><label>Bare</label> </MDBCol>	
-    <MDBCol><input type = "radio" name = "format" value = "Header"  onChange={e => setFormat(e.target.value)} /><label>Header</label> </MDBCol>	
-    <MDBCol><input type = "radio" name = "format" value = "JSON"  onChange={e => setFormat(e.target.value)} /><label>JSON</label> </MDBCol>	
-    <MDBCol><input type = "radio" name = "format" value = "JSON_Compact" onChange={e => setFormat(e.target.value)} /><label>JSON_Compact</label> </MDBCol>
-    <MDBCol><input type = "radio" name = "format" value = "Pretty" onChange={e => setFormat(e.target.value)} /><label>Pretty</label> </MDBCol>
+    <MDBRow>
+    <MDBCol><div className="custom-control custom-radio"><input type = "radio" name = "format" value = "Bare" className= "custom-control-input" id="defaultGroupExample3" onChange={formatChange}/><label className="custom-control-label" for="defaultGroupExample3">Bare</label></div> </MDBCol>	
+    <MDBCol><div className="custom-control custom-radio"><input type = "radio" name = "format" value = "Header"  className= "custom-control-input" id="defaultGroupExample4" onChange={formatChange} /><label className="custom-control-label" for="defaultGroupExample4">Header</label></div> </MDBCol>	
+    </MDBRow>
+    &nbsp;&nbsp;&nbsp;
+    <MDBRow>
+    <MDBCol><div className="custom-control custom-radio"><input type = "radio" name = "format" value = "JSON"  className= "custom-control-input" id="defaultGroupExample5" onChange={formatChange} /><label className="custom-control-label" for="defaultGroupExample5">JSON</label></div> </MDBCol>	
+    <MDBCol><div className="custom-control custom-radio"><input type = "radio" name = "format" value = "JSON_Compact" className= "custom-control-input" id="defaultGroupExample6" onChange={formatChange} /><label className="custom-control-label" for="defaultGroupExample6" >JSON_Compact</label></div> </MDBCol>
+    </MDBRow>
+    &nbsp;&nbsp;&nbsp;
+    <div id="next"></div>
+    <MDBCol><div className="custom-control custom-radio"><input type = "radio" name = "format" value = "Pretty" className= "custom-control-input" id="defaultGroupExample7" onChange={formatChange} /><label className="custom-control-label" for="defaultGroupExample7">Pretty</label></div> </MDBCol>
     </fieldset>
+    <div className="btn1">
     <MDBBtn outline color="info" type = "submit">Submit</MDBBtn>
+    </div>
     </form>
     </MDBContainer>
     
     </div>
+    <div style={{float:'right'}} className="next">
     <Button name= "Next"></Button>
+    </div>
     </div>
 
    );
