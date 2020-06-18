7bfeceedd5f6d033c01f088d2822632a332fabb9
momolin78 <momolin78@gmail.com>
2020-06-18 17:43:27 -0400

    moving wrapper into a seperate folder to test if tests work

--- web/api/backend.go
+++ web/api/backend.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"net/http"
 	"time"
-
+	wrapper "github.com/google/oauth2l/web/api/wrapper"
 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	"github.com/dgrijalva/jwt-go"
 )
@@ -98,7 +98,7 @@ func NoTokenHandler(w http.ResponseWriter, r *http.Request) {
 //OkHandler function to test is token in valid
 func OkHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
-	newWrapperCommand := &WrapperCommand{
+	newWrapperCommand := &wrapper.WrapperCommand{
 		RequestType: creds.RequestType,
 		Args:        creds.Args,
 	}
