b2804082ce5e1f0766f324cdec0edb49e1a9b4c2
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-06-18 16:31:38 -0500

    Add even more tests

--- web/api/wrapper_test.go
+++ web/api/wrapper_test.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"reflect"
 	"testing"
-	"encoding/json"
 )
 
 
@@ -92,5 +93,61 @@ func TestJSONValidTypeInArgs(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("valid types not detected")
+	}
+}
+
+func TestJSONInvalidTypeInArgs(t *testing.T) {
+	wrapper := WrapperCommand{}
+
+	jsonString := []byte(`{
+        "requesttype": "fetch",
+        "args": {
+            "--scope": 2
+		},
+		"body": {}
+	}`)
+	
+	json.Unmarshal(jsonString, &wrapper)
+
+	_, err := wrapper.Execute()
+
+	if err.Error() != "invalid type found in args" {
+		t.Errorf("invalid types not detected")
+	}
+}
+
+func TestJSONValueInArgs(t *testing.T) {
+	wrapper := WrapperCommand{}
+
+	jsonString := []byte(`{
+        "requesttype": "fetch",
+        "args": {
+            "--scope": ["cloud-platform", "userinfo.email"]
+		},
+		"body": {}
+	}`)
+	
+	json.Unmarshal(jsonString, &wrapper)
+
+	flattenedArgs, _ := combinedArgs(wrapper)
+
+	if !reflect.DeepEqual(flattenedArgs, []string{"fetch", "--scope", "cloud-platform", "userinfo.email"}) {
+		t.Errorf("flattened values not correct")
+	}
+}
+
+func TestDummyOauth2lCommand(t *testing.T) {
+	const expectedRequest = "test"
+	var expectedArgs = Args{"--token": "ya29.justkiddingmadethisoneup"}
+
+	wrapper := WrapperCommand{
+		expectedRequest,
+		expectedArgs,
+	}
+
+	output, err := wrapper.Execute()
+	fmt.Printf("%v", []byte(output))
+	if output != "1" || err != nil {
+		t.Errorf("error running basic command")
 	}
 }
