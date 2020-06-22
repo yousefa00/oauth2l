a613a6a3ad743c40741cbc1a7bebb810cb847676
momolin78 <momolin78@gmail.com>
2020-06-22 19:43:31 -0400

    changing vscode auto format

--- web/client/src/Scopes.js
+++ web/client/src/Scopes.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 import CheckboxList from "./components/Checklist";
@@ -9,47 +10,39 @@ import CheckboxList from "./components/Checklist";
 function Scopes() {
     const [scope, setScope] = useState("");
 
-    const handleSubmit = (e) => {
-        return [scope];
-    };
+  const handleSubmit = (e) => {
+      return [scope];
+  };
 
-    return ( <
-        div className = "top" >
-        <
-        div className = "shadow-box-example z-depth-2" >
-        <
-        MDBContainer >
-        <
-        form onSubmit = { handleSubmit } >
-        <
-        MDBCol > { " " } <
-        p className = "h5 text-center mb-4" > Choose token type < /p> <
-        /MDBCol>
+  return (
+    <div className="top">
+      <div className="shadow-box-example z-depth-2">
+        <MDBContainer>
+          <form onSubmit={handleSubmit}>
+            <MDBCol>
+              {" "}
+              <p className="h5 text-center mb-4">Choose token type </p>
+            </MDBCol>
 
-        <
-        MDBRow >
-        <
-        MDBCol > { " " } <
-        CheckboxList >
-        onChange = {
-            (e) => setScope(e.target.value) } <
-        /CheckboxList> <
-        /MDBCol> <
-        /MDBRow>
-
-        <
-        div className = "next" >
-        <
-        MDBBtn outline color = "info"
-        type = "submit" >
-        Submit <
-        /MDBBtn> <
-        /div> <
-        /form> <
-        /MDBContainer> <
-        /div> <
-        /div>
-    );
+           <MDBRow>
+           <MDBCol>
+                  {" "}
+                  <CheckboxList>
+                  onChange={(e) => setScope(e.target.value)}
+                  </CheckboxList>
+                </MDBCol>
+           </MDBRow>
+          
+            <div className="next">
+              <MDBBtn outline color="info" type="submit">
+                Submit
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBContainer>
+      </div>
+    </div>
+  );
 }
 
 export default Scopes;
