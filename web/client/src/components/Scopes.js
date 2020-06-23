aa1be64b21fa0afb19a8ffe464264d62bef0f7b4
momolin78 <momolin78@gmail.com>
2020-06-23 14:44:14 -0400

    saving input to a state in Scopes

--- web/client/src/components/Scopes.js
+++ web/client/src/components/Scopes.js
@@ -3,30 +3,74 @@ import { MDBContainer, MDBCol, MDBBtn, MDBInput } from "mdbreact";
 
 /**
  *
- * @return {div} returns the page that contains the ability to choose the scopes
+ * Class that represents the page that contains the ability to choose the scopes
  */
-function Scopes() {
-  return (
-    <div className="top">
-      <div className="shadow-box-example z-depth-2">
-        <MDBContainer>
-          <form>
-            <MDBCol>
-              {" "}
-              <p className="h5 text-center mb-4">Enter scopes </p>
-            </MDBCol>
-            <MDBInput id="scopes-label" label="Scopes" size="lg" />
+class Scopes extends React.Component {
+  /**
+   *
+   * @param {*} props holds information about the scopes array
+   * constructor for Scope
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      scopes: [],
+    };
 
-            <div className="next">
-              <MDBBtn outline color="info" type="submit">
-                Submit
-              </MDBBtn>
-            </div>
-          </form>
-        </MDBContainer>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /**
+   *
+   * @param {*} event
+   * function to handle changes to the input
+   */
+  handleChange(event) {
+    this.setState({ scopes: event.target.value.split(" ") });
+  }
+
+  /**
+   *
+   * @param {*} event
+   * @return {*} string array with scopes in it
+   */
+  handleSubmit(event) {
+    event.preventDefault();
+    return this.state.scopes;
+  }
+
+  /**
+   * @return {div} scopes component
+   */
+  render() {
+    return (
+      <div className="top">
+        <div className="shadow-box-example z-depth-2">
+          <MDBContainer>
+            <form onSubmit={this.handleSubmit}>
+              <MDBCol>
+                {" "}
+                <p className="h5 text-center mb-4">Enter scopes </p>
+              </MDBCol>
+              <MDBInput
+                id="scopes-label"
+                label="Scopes"
+                size="lg"
+                onChange={this.handleChange}
+              />
+
+              <div className="next">
+                <MDBBtn outline color="info" type="submit">
+                  Submit
+                </MDBBtn>
+              </div>
+            </form>
+          </MDBContainer>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Scopes;
